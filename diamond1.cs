fab63b21dcb6b47d66b2ed800b125f806220b9cf
ashok <wchild30@gmail.com>
2015-09-05 18:45:46 +0530

    Create diamond1.cs

---
+++ diamond1.cs
@@ -0,0 +1,36 @@
+using System;
+    class Program
+    {
+        static int a = 5;
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= a; i++)
+            {
+                for (int k = a; k >= i; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = a ; i >= 1; i--)
+            {
+                for (int k = a; k >= i; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+
+                    Console.Write("* ", i);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
